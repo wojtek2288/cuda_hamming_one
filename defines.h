5d8ccfe39c7cc871bec80a2d28f8848064b91a55
Wojciech <wojtek2288@gmail.com>
2023-01-21 12:04:40 +0100

    remove threads blocks from defines

--- defines.h
+++ defines.h
@@ -1,5 +1,3 @@
-#define THREADS 512
-#define BLOCKS 1024
 #define ERR(source) (perror(source),                                 \
                      fprintf(stderr, "%s:%d\n", __FILE__, __LINE__), \
                      exit(EXIT_FAILURE))
