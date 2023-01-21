ed14db47125314b633aa75748f64d4873e11eb7d
Wojciech <wojtek2288@gmail.com>
2023-01-21 13:36:00 +0100

    final

--- cpu.cpp
+++ cpu.cpp
@@ -12,14 +12,14 @@ using namespace std;
 // https://stackoverflow.com/questions/27086195/linear-index-upper-triangular-matrix
 int solveWithCpu(vector<string> bitSequences)
 {
-    int n = bitSequences.size();
-    int len = bitSequences[0].length();
+    unsigned long long int n = bitSequences.size();
+    unsigned long long int len = bitSequences[0].length();
     int pairs = 0;
 
-    for (int k = 0; k < n * (n - 1) / 2; k++)
+    for (unsigned long long int k = 0; k < n * (n - 1) / 2; k++)
     {
         unsigned long long int i = n - 2 - floor(sqrt(-8 * k + 4 * n * (n - 1) - 7) / 2.0 - 0.5);
-        unsigned long long int j = k + i + 1 - n * (n - 1) / 2 + (n - i) * ((n - i) - 1) / 2;
+        unsigned long long int j = k + i + 1 - n * (n - 1) / 2.0 + (n - i) * ((n - i) - 1) / 2.0;
 
         int hammingDistance = 0;
 
--- generator.cpp
+++ generator.cpp
@@ -14,30 +14,14 @@ int main(int argc, char **argv)
     std::string file = argv[3];
     std::ofstream out(file);
 
-    std::string previousVector = "";
-
     for (int i = 0; i < vectorCount; i++)
     {
-        if (i % 15 == 0 && i > 0)
+        for (int j = 0; j < vectorLength; j++)
         {
-            int random_bit = rand() % vectorLength;
-            previousVector[random_bit] = previousVector[random_bit] == '0' ? '1' : '0';
-            out << previousVector << std::endl;
-            previousVector = "";
+            char current_bit = (rand() % 2) + '0';
+            out << current_bit;
         }
-        else
-        {
-            for (int j = 0; j < vectorLength; j++)
-            {
-                char current_bit = (rand() % 2) + '0';
-                out << current_bit;
-                if (i % 14 == 0 && i > 0)
-                {
-                    previousVector += current_bit;
-                }
-            }
-            out << std::endl;
-        }
+        out << std::endl;
     }
 
     out.close();
