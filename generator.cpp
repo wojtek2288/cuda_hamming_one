d3122d2602a95dab59d5ec0aa3b98f5f7fb6239b
Wojciech <wojtek2288@gmail.com>
2023-01-22 20:28:32 +0100

    ad

--- generator.cpp
+++ generator.cpp
@@ -14,14 +14,30 @@ int main(int argc, char **argv)
     std::string file = argv[3];
     std::ofstream out(file);
 
+    std::string previousVector = "";
+
     for (int i = 0; i < vectorCount; i++)
     {
-        for (int j = 0; j < vectorLength; j++)
+        if (i % 15 == 0 && i > 0)
         {
-            char current_bit = (rand() % 2) + '0';
-            out << current_bit;
+            int random_bit = rand() % vectorLength;
+            previousVector[random_bit] = previousVector[random_bit] == '0' ? '1' : '0';
+            out << previousVector << std::endl;
+            previousVector = "";
         }
-        out << std::endl;
+        else
+        {
+            for (int j = 0; j < vectorLength; j++)
+            {
+                char current_bit = (rand() % 2) + '0';
+                out << current_bit;
+                if (i % 14 == 0 && i > 0)
+                {
+                    previousVector += current_bit;
+                }
+            }
+            out << std::endl;
+        }
     }
 
     out.close();
